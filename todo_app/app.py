6ad90eaf3670a72e34129ca5feb4234770d03191
Haseeb Mahmood <Haseebmahmood00@gmail.com>
2021-02-26 16:31:06 +0000

    Got the post new item to work
    removed doing list so now we have todo and completed

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, redirect, request, Response
-import todo_app.data.session_items as session
 from todo_app.flask_config import Config
 # This code sample uses the 'requests' library:
 # http://docs.python-requests.org
@@ -23,29 +22,39 @@ def index():
     params=query
     )
 
+
+    
+
     trello_items = (response.json()) #declare variable and add to the line below items_list
     for item in trello_items:
         if item["idList"] == '5ff3a8b1997284580b7a4fcc':
             item["status"] = "Done"
-
-        elif item["idList"] == '5ff3a8b1997284580b7a4fcb':
-            item["status"] = "Doing"
-
         else: item["status"] = "Todo"
     return render_template("index.html", items = trello_items)
 
 @app.route('/addItem', methods =["POST"])
 def add():
-    title = request.form.get('new_todo_item')
-    session.add_item(title)
+    name = request.form.get('new_todo_item') 
+    url = "https://api.trello.com/1/cards"
+    query = {
+    'key': os.getenv("TRELLO_KEY"),
+    'token': os.getenv("TRELLO_TOKEN"),
+    'idList': os.getenv("TRELLO_TODO_IDLIST"),
+    'name': name
+    }
+    response = requests.request(
+    "POST",
+    url,
+    params=query
+    )
     return redirect ("/")
 
 @app.route('/completeItem/<id>', methods =["POST"])
 def complete_item(id):
     converted_id = int(id)
-    item = session.get_item(converted_id)
+    item = request.get_item(converted_id)
     item["status"] = "Completed"
-    session.save_item(item)
+    request.save_item(item)
     return redirect ("/")
 
 if __name__ == '__main__':
